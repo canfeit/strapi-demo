1d7e9d89d3c4ecbd61578ce47ffe1f23a61c9e97
canfeit <canfeit@qq.com>
2019-01-31 14:03:18 +0800

    add api

---
+++ api/documents/config/sample.md
@@ -0,0 +1,14 @@
+## mvc+api
+
+routes
+--user.js
+--cart.js
+controllers
+--user.js
+--cart.js
+services
+--user.js
+--cart.js
+models(ORM)
+--user.js
+--cart.js
---
+++ api/documents/controllers/Documents.js
@@ -0,0 +1,78 @@
+'use strict';
+
+/**
+ * Documents.js controller
+ *
+ * @description: A set of functions called "actions" for managing `Documents`.
+ */
+
+module.exports = {
+
+  /**
+   * Retrieve documents records.
+   *
+   * @return {Object|Array}
+   */
+
+  find: async (ctx) => {
+    if (ctx.query._q) {
+      return strapi.services.documents.search(ctx.query);
+    } else {
+      return strapi.services.documents.fetchAll(ctx.query);
+    }
+  },
+
+  /**
+   * Retrieve a documents record.
+   *
+   * @return {Object}
+   */
+
+  findOne: async (ctx) => {
+    if (!ctx.params._id.match(/^[0-9a-fA-F]{24}$/)) {
+      return ctx.notFound();
+    }
+
+    return strapi.services.documents.fetch(ctx.params);
+  },
+
+  /**
+   * Count documents records.
+   *
+   * @return {Number}
+   */
+
+  count: async (ctx) => {
+    return strapi.services.documents.count(ctx.query);
+  },
+
+  /**
+   * Create a/an documents record.
+   *
+   * @return {Object}
+   */
+
+  create: async (ctx) => {
+    return strapi.services.documents.add(ctx.request.body);
+  },
+
+  /**
+   * Update a/an documents record.
+   *
+   * @return {Object}
+   */
+
+  update: async (ctx, next) => {
+    return strapi.services.documents.edit(ctx.params, ctx.request.body) ;
+  },
+
+  /**
+   * Destroy a/an documents record.
+   *
+   * @return {Object}
+   */
+
+  destroy: async (ctx, next) => {
+    return strapi.services.documents.remove(ctx.params);
+  }
+};
